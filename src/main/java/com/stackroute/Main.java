ddd3acf20905ce9296ead83252f5bad2ca9bef61
akhila <bakhila36@gmail.com>
2019-07-11 22:50:49 +0530

    second task

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -14,26 +14,34 @@ import org.springframework.core.AliasRegistry;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args)
     {
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie=context.getBean("movie",Movie.class);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie=(Movie)applicationContext.getBean("movie1");
+        Movie movie2=(Movie)applicationContext.getBean("movie2");
         movie.acting();
+        movie2.acting();
+
+
+        System.out.println(movie==movie2);
+
 
 //        Actor actor=(Actor) context.getBean("actor",Actor.class);
 //        actor.displayActorInfo();
 
-        ClassPathResource resource=new ClassPathResource("beans.xml");
-        BeanFactory factory=new XmlBeanFactory(resource);
-        Movie movie1=context.getBean("movie",Movie.class);
-        movie1.acting();
-
-        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
-        BeanDefinitionReader reader=new XmlBeanDefinitionReader(beanFactory);
-        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
-        Movie movie2=context.getBean("movie",Movie.class);
-        movie2.acting();
+//        ClassPathResource resource=new ClassPathResource("beans.xml");
+//        BeanFactory factory=new XmlBeanFactory(resource);
+//        Movie movie1=context.getBean("movie",Movie.class);
+//        movie1.acting();
+//
+//        DefaultListableBeanFactory beanFactory=new DefaultListableBeanFactory();
+//        BeanDefinitionReader reader=new XmlBeanDefinitionReader(beanFactory);
+//        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/beans.xml"));
+//        Movie movie2=context.getBean("movie",Movie.class);
+//        movie2.acting();
 
 
 
--- src/main/java/com/stackroute/spring/Movie.java
+++ src/main/java/com/stackroute/spring/Movie.java
@@ -4,14 +4,19 @@ public class Movie
 {
     private Actor actor;
 
+    public Movie(Actor actor) {
+        this.actor = actor;
+    }
+
     public void setActor(Actor actor) {
         this.actor = actor;
     }
+
+    public Actor getActor() {
+        return actor;
+    }
     public void acting()
     {
-        System.out.println(actor.getName());
-        System.out.println(actor.getAge());
-        System.out.println(actor.getGender());
-
+        System.out.println("Actor name is "+actor.getName()+" actor gender is "+actor.getGender()+" actor age is "+actor.getAge());
     }
 }
